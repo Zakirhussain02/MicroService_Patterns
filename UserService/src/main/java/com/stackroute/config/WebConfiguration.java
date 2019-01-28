e5157d53d9d763ec40368e35c01425afb49a8f98
hima <himacp2396@gmail.com>
2019-01-28 14:11:24 +0530

    edited controller

--- UserService/src/main/java/com/stackroute/config/WebConfiguration.java
+++ UserService/src/main/java/com/stackroute/config/WebConfiguration.java
@@ -17,6 +17,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static jdk.internal.joptsimple.util.RegexMatcher.regex;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
