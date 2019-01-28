11ca433bef740cad5624dd460cdf061f8a26709a
Neethu <neethubabu.babu0@gmail.com>
2019-01-28 13:11:46 +0530

    added service interface

--- UserService/src/main/java/com/stackroute/service/UserService.java
+++ UserService/src/main/java/com/stackroute/service/UserService.java
@@ -1,4 +1,14 @@
 package com.stackroute.service;
 
-public class UserService {
+import com.stackroute.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    public User saveMuzix(User muzix)throws UserAlreadyExistsException;
+    public List<User> getAllMuzixs()throws UserNotFoundException;
+    public User updateMuzix(int trackId,String comment)throws UserNotFoundException;
+    public List<User> removeMuzix(int trackId) throws UserNotFoundException;
+    public User trackByTrackId(int trackId) throws UserNotFoundException;
+    public List<User> trackByTrackName(String trackName)throws UserNotFoundException;
 }
